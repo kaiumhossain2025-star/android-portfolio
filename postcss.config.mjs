27952e96154d19ced7fff72ff441fc6b22499c2a
smansayeed <786saadman@gmail.com>
2025-12-30 23:33:50 +0600

    Fixed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,7 @@
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://kdxckihjekccmaxywpsn.supabase.co'
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkeGNraWhqZWtjY21heHl3cHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMDcwNzgsImV4cCI6MjA4MjY4MzA3OH0.R_qbMxaDWKE3HRNl9rFkIPDy-rRzroXU2hKgF6lBVm8'
+process.env.SUPABASE_SERVICE_ROLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkeGNraWhqZWtjY21heHl3cHNuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NzEwNzA3OCwiZXhwIjoyMDgyNjgzMDc4fQ.2fsGVhwFvF-4vhhGrxT4Q_Q4wt59Lr9Vwh16jgdyh7s'
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
